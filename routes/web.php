46859a431abfe1d5667e38a5e68428744c205c2d
Ahmad Rifki Fauzi <ahmadrifki3403@gmail.com>
2023-02-20 09:29:34 +0700

    Praktikum_2-ControllerWebFrameworkLaravel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +20,18 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    echo "Selamat Datang";
-});
+// Route::get('/', function () {
+//     echo "Selamat Datang";
+// });
 
-Route::get('/about', function () {
-    echo "2141720028 - Ahmad Rifki Fauzi";
-});
+// Route::get('/about', function () {
+//     echo "2141720028 - Ahmad Rifki Fauzi";
+// });
 
-Route::get('/articles/{id}', function ($id) {
-    echo "Halaman Artikel dengan ID ".$id;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     echo "Halaman Artikel dengan ID ".$id;
+// });
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
